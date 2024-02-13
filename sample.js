065d106b62897c8b5905480b1b9176cedc7e6833
ResulAK <145531876+ResulAK@users.noreply.github.com>
2024-02-13 16:19:54 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -2,3 +2,32 @@
 
 
 console.log("Hello");
+
+function setCookie(cname, cvalue, exdays, domain, path) {
+    console.log("Set cookie called");
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    let cookieString = cname + "=" + cvalue + ";" + expires;
+
+    console.log(cookieString);
+    // Add domain if provided
+    if (domain) {
+        cookieString += "; domain=" + domain;
+    }
+
+    // Add path if provided
+    if (path) {
+        cookieString += "; path=" + path;
+    } else {
+        cookieString += "; path=/"; // Default path
+    }
+
+    console.log(cookieString);
+    document.cookie = cookieString;
+}
+
+setTimeout(() => {
+    console.log("Setting cookie");
+    setCookie("Anand", "Krishna", 3, ".resulak.github.io");
+}, 3000);
